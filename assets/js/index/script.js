50a99d372351e598b7391e0a9f57cd69d5323ceb
HuuBang11 <huubang0312@gmail.com>
2025-11-11 14:05:45 +0700

    update code

--- assets/js/index/script.js
+++ assets/js/index/script.js
@@ -2,16 +2,79 @@ import { preloadImages } from "../../libs/utils.js";
 ("use strict");
 $ = jQuery;
 // setup lenis
-const lenis = new Lenis();
-lenis.on("scroll", ScrollTrigger.update);
-gsap.ticker.add((time) => {
-  lenis.raf(time * 1000);
-});
 
-gsap.ticker.lagSmoothing(0);
+// const lenis = new Lenis();
+// lenis.on("scroll", ScrollTrigger.update);
+// gsap.ticker.add((time) => {
+//   lenis.raf(time * 1000);
+// });
+// gsap.ticker.lagSmoothing(0);
+
 // end lenis
+
+function magicCursor() {
+  if ($(window).width < 1024) return;
+
+  var circle = document.querySelector(".magic-cursor");
+
+  gsap.set(circle, {
+    xPercent: -50,
+    yPercent: -50
+  });
+
+  let mouseX = 0,
+    mouseY = 0;
+
+  window.addEventListener("mousemove", (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+
+    gsap.to(circle, {
+      x: mouseX,
+      y: mouseY,
+      duration: 0.1
+    });
+  });
+
+  const items = document.querySelectorAll(".modal, [data-cursor-text]");
+  var cursorDot = document.querySelector(".magic-cursor .cursor");
+  var cursorText = document.querySelector(".magic-cursor .cursor .text");
+
+  items.forEach((item) => {
+    item.addEventListener("mouseenter", () => {
+      let text = "";
+      if (item.classList.contains("modal")) {
+        text = "Đóng";
+      } else {
+        text = item.getAttribute("data-cursor-text");
+      }
+
+      // const text = item.getAttribute("data-cursor-text");
+      cursorText.innerHTML = `<span class="color-white">${text}</span>`;
+      cursorDot.classList.add("show-text");
+    });
+
+    item.addEventListener("mouseleave", () => {
+      cursorText.innerHTML = "";
+      cursorDot.classList.remove("show-text");
+    });
+  });
+
+  const itemsContent = document.querySelectorAll(".modal-dialog");
+  itemsContent.forEach((item) => {
+    item.addEventListener("mouseenter", () => {
+      cursorDot.classList.remove("show-text");
+    });
+    item.addEventListener("mouseleave", () => {
+      cursorText.innerHTML = `<span class="color-white">Đóng</span>`;
+      cursorDot.classList.add("show-text");
+    });
+  });
+}
+
 const init = () => {
   gsap.registerPlugin(ScrollTrigger);
+  magicCursor();
 };
 preloadImages("img").then(() => {
   // Once images are preloaded, remove the 'loading' indicator/class from the body
